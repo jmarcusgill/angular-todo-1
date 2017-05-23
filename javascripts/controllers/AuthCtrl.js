982ca344b3562dd6aa36856b5a781feb449410e6
Zoe Ames <zoeames@gmail.com>
2017-05-22 20:10:41 -0500

    logout working

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -4,6 +4,12 @@ app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, 
 		password: "123456"
 	};
 	
+	if($location.path() === '/logout'){
+		AuthFactory.logout();
+		$rootScope.user = {};
+		$location.url('/auth');
+	}
+
 	let logMeIn = () =>{
 		AuthFactory.authenticate($scope.auth).then((userCreds) => {
 			return UserFactory.getUser(userCreds.uid);
