15b1c1f6f734e28356c6a7842dba5215ec0f3c6a
Zoe Ames <zoeames@gmail.com>
2017-05-22 20:02:17 -0500

    login and register working

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -1,23 +1,36 @@
-app.controller("AuthCtrl", function($scope, AuthFactory, UserFactory){
-	$scope.auth = {};
+app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, UserFactory){
+	$scope.auth = {
+		email: "a@a.com",
+		password: "123456"
+	};
 	
+	let logMeIn = () =>{
+		AuthFactory.authenticate($scope.auth).then((userCreds) => {
+			return UserFactory.getUser(userCreds.uid);
+		}, (error) => {
+			console.log("authenticate error", error);
+		}).then((user) => {
+			$rootScope.user = user;
+			$location.url('/items/list');
+		}).catch((error) => {
+			console.log("getUser error", error);
+		});
+	};
+
 	$scope.registerUser = () => {
-		// adding username
-		//login
 		AuthFactory.registerWithEmail($scope.auth).then((didRegister) => {
 			$scope.auth.uid = didRegister.uid;
 			return UserFactory.addUser($scope.auth);
 		}, (error) => {
 			console.log("registerWithEmail error", error);
 		}).then((registerComplete) => {
-			console.log("registerComplete", registerComplete);
+			logMeIn();
 		}).catch((error) => {
 			console.log("addUser error", error);
 		});
 	};
 
 	$scope.loginUser = () => {
-
+		logMeIn();
 	};
-
 });
